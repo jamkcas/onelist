6f02204b0da32cb812a2eaaa9c295cdf7d5b085c
james <jamkcas@yahoo.com>
2014-01-28 19:29:04 -0800

    added simple search by title feature, and ng-click button to add new items to list view

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -3,21 +3,34 @@ var app = angular.module('oneListApp', ['ngRoute']);
 app.config(function($routeProvider) {
   $routeProvider.when('/complete',
     {
-      controller: 'listController',
+      controller: 'completeController',
       templateUrl: 'templates/complete_list.html'
     })
     .when('/incomplete',
     {
-      controller: 'listController',
+      controller: 'incompleteController',
       templateUrl: 'templates/incomplete_list.html'
     })
-    .otherwise({ redirectTo: '/complete' });
+    .otherwise({ redirectTo: '/incomplete' });
 });
 
-app.controller('listController', function($scope) {
+app.controller('completeController', function($scope) {
   $scope.items = [
     { title: 'shop', note: 'buy groceries' },
     { title: 'job', note: 'look for job' },
     { title: 'clean', note: 'clean house' }
   ];
+});
+
+app.controller('incompleteController', function($scope) {
+  $scope.items = [
+    { title: 'eat', note: 'eat breakfast' },
+    { title: 'cats', note: 'feed cats' },
+    { title: 'dog', note: 'feed dog' }
+  ];
+
+  $scope.addItem = function() {
+    $scope.items.push({ title: $scope.newItem.title, note: $scope.newItem.note });
+    console.log($scope.items)
+  }
 });
